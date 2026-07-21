91ca13270a40b4135f03fc97c904a1615b3a2de4
Prakash Kumar <prakashpandeyec032@gmail.com>
2026-07-22 00:32:12 +0530

    added duckdns

--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -1,7 +1,7 @@
 import { FileText, FileCode, FileSpreadsheet } from 'lucide-react';
 import React from 'react';
 
-export const API_URL = 'http://165.22.216.100:6005';
+export const API_URL = 'https://mopabot.duckdns.org';
 
 // Helper to extract YouTube video ID and fetch HQ thumbnail
 export const getYouTubeThumbnail = (url) => {
